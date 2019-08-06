c44a413dfb4ba172246c30a84b9597aaf647b602
Kyle Ruff <kyleruff@gmail.com>
2019-08-05 20:42:31 -0600

    get and post

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,17 +1,40 @@
 var path = require("path");
-
+var tables = [
+    {
+    routeName: "duderoute",
+    name: "dude",
+    email: "dude@email.com",
+    time: "10am"
+    }];
 module.exports = function(app) {
 
   app.get("/tables", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/tables.html"));
+    res.sendFile(path.join(__dirname, "../tables.html"));
   });
 
   app.get("/reserve", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/reserve.html"));
+    res.sendFile(path.join(__dirname, "../reserve.html"));
   });
 
   // If no matching route is found default to home
   app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/home.html"));
+    res.sendFile(path.join(__dirname, "../home.html"));
   });
+
+  // Create New Reservations - takes in JSON input
+app.post("/api/tables", function(req, res) {
+    // req.body hosts is equal to the JSON post sent from the user
+    // This works because of our body parsing middleware
+    var newReservation = req.body;
+    newReservation.routeName = newReservation.name.replace(/\s+/g, "").toLowerCase();
+
+    console.log(newReservation);
+  
+    tables.push(newReservation);
+  
+    res.json(newReservation);
+  });
+  
+  
 };
+
